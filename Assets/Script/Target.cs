2d8fac693d8bccdab62ea211e6bdd2aa5bb92dbc
DarshaanSingh <142627516+DarshaanSingh@users.noreply.github.com>
2024-02-23 13:22:58 -0800

    Revert "5.2"
    This reverts commit 258edcb8d3feae136c3534fb8c4261d132769fa1.

--- Assets/Script/Target.cs
+++ Assets/Script/Target.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public ParticleSystem explosionParticle;
-    public int pointValue;
     private Rigidbody targetRb;
     private float minSpeed = 12.0f;
     private float maxSpeed = 16.0f;
     private float maxTorque = 10.0f;
     private float xRange = 4.0f;
     private float ySpawnPos = -6.0f;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +20,6 @@ public class Target : MonoBehaviour
         RandomTorque(),
         ForceMode.Impulse);
         transform.position = RandomSpawnPos();
-        
     }
 
 
@@ -42,8 +38,6 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        GameManager.instance.UpdateScore(pointValue);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
     }
     private void OnTriggerEnter(Collider other)
     {
